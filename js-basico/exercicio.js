f0f3e63b8919bafbd1e52d65d84fda9f5e27378a
Victor Machado <victormachadohub@gmail.com>
2026-09-16 22:20:19 -0300

    Comitei

--- js-basico/exercicio.js
+++ js-basico/exercicio.js
@@ -1,9 +1,57 @@
 const personagens = [
-    {nome: "Kael", idade: 12},
-    {nome: "Nyxen", idade: 23},
-    {nome: "Kanner", idade: 132}
+    { nome: "Kael", idade: 12 },
+    { nome: "Nyxen", idade: 23 },
+    { nome: "Kanner", idade: 132 }
 ];
 
+
+// personagens.splice(0);
+
+personagens.push("Arroz");
+console.log(personagens);
+// const nomesprimeiro = personagens.slice(1);
+
+// console.log(nomesprimeiro)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Exemplo: acessar diretamente a posição 2
+// let posicao = 2;
+
+
+// // Encontrar personagem pelo nome
+// posicao = personagens.findIndex(p => p.nome === "Nyxen");
+// if (posicao !== -1) {
+//     console.log(`Nyxen está na posição ${posicao}`);
+// } else {
+//     console.log("Nyxen não encontrado.");
+// }
+
+
+// for (let i = 0; i < personagens.length; i++){
+//     const idade = personagens[i];
+//     if(idade.idade > 20) {
+//         console.log(idade);
+//         break;
+//     } 
+
+// const seulugar = personagens.findIndex((personagens) => personagens == personagens);
+
+// console.log(seulugar); // 1
 
 // const maioridade = personagens.find((personagens) => personagens.idade > 30);
 
@@ -27,11 +75,11 @@ const personagens = [
 //     }
 // };
 
-for (let i = 0; i < personagens.length; i++) {
-    console.log(`[INDEX ATUAL] ${i}`);     // agora com crase e i minúsculo
-    let personagem = personagens[i];       // colchetes []
-    console.log(personagem);
-}
+// for (let i = 0; i < personagens.length; i++) {
+//     console.log(`[INDEX ATUAL] ${i}`);     // agora com crase e i minúsculo
+//     let personagem = personagens[i];       // colchetes []
+//     console.log(personagem);
+// }
 
 
 
--- minha primeira api/index.js
+++ minha primeira api/index.js
@@ -1,15 +1,49 @@
-import express from 'express'
+import express from 'express';
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send("dede")
+
+// app.get("/produtos", (req, res) => {
+//     const { categoria } = req.query;
+//     const resultado = produtos.filter((produto) => produto.categoria === categoria);
+//     res.json(resultado);
+// });
+let livros = [
+    {
+        idLivro: 1,
+        idTitulo: "as cronicas",
+        idAutor: "Autozin",
+
+    }
+]
+
+
+
+app.get("/", function(req,res){
+    res.send("Seja bem vindo a gestao de livros");
 });
 
-app.get("/livros", function(pergunta, resposta) {
-
-    console.log('angelo hankfenrfioenr')
-    resposta.send("Hello Wrold");
+app.get("/livros", function(req, res){
+    res.json(livros);
 });
 
-app.listen(3000);
+app.get("/livros/:id", (req, res) => {
+    console.log(res.params.id);
+    const id = parseInt(req.params.id);
+
+    if (isNaN(id)) {
+        console.log("caiuaqui")
+        .status(400)
+        .json({mensagem:"dada"})
+    }
+
+});
+
+
+
+app.listen(3000, () => {
+    console.log("Servidor rodando na porta 3000");
+});
+
+// URL certa pra testar:
+// http://localhost:3000/produtos?categoria=eletronicos
