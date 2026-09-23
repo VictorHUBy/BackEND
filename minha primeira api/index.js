06690f2e0267656eac30295e4c0943cd72872d8f
Victor Machado <victormachadohub@gmail.com>
2026-09-23 20:46:22 -0300

    Atualiza API de livros

--- minha primeira api/index.js
+++ minha primeira api/index.js
@@ -1,49 +1,88 @@
-import express from 'express';
+import express from "express";
 
-const app = express();
+const app = express(); // primeiro pilar: instancia do express
+app.use(express.json());
 
+/**
+ * idLivro -> identificador / int
+ * dsTitulo -> string
+ * dsAutor -> string
+ * fgDisponivel -> boolean
+ */
 
-// app.get("/produtos", (req, res) => {
-//     const { categoria } = req.query;
-//     const resultado = produtos.filter((produto) => produto.categoria === categoria);
-//     res.json(resultado);
-// });
+let ultimo_id = 1;
+
 let livros = [
     {
         idLivro: 1,
-        idTitulo: "as cronicas",
-        idAutor: "Autozin",
+        dsTitulo: "as cronicas de narnia",
+        dsAutor: "C S Lewis",
+        fgDisponivel: true,
+    },
+]; // banco de dados
 
-    }
-]
-
-
-
-app.get("/", function(req,res){
-    res.send("Seja bem vindo a gestao de livros");
+app.get("/", function (req, res) {
+    res.send("seja bem vindo à gestao de livros");
 });
 
-app.get("/livros", function(req, res){
+app.get("/livros", function (req, res) {
+    console.log("chamando rota GET /livros");
     res.json(livros);
 });
 
 app.get("/livros/:id", (req, res) => {
-    console.log(res.params.id);
+    console.log(req.params.id);
+
     const id = parseInt(req.params.id);
 
     if (isNaN(id)) {
-        console.log("caiuaqui")
-        .status(400)
-        .json({mensagem:"dada"})
+        // se nao for um numero
+        return res
+            .status(400) // requisicao mal formada
+            .json({ mensagem: "o parametro precisa ser um numero valido" });
     }
 
+    let livro = livros.find((livro) => {
+        return livro.idLivro === id;
+    });
+
+    if (!livro) {
+        return res.status(404).send();
+    }
+
+    res.json(livro);
 });
 
+app.post("/livros", (req, res) => {
+    let autor_enviado = req.body.dsAutor;
+    let titulo_enviado = req.body.dsTitulo;
 
+    console.log(autor_enviado);
+    console.log(titulo_enviado);
+
+    if (!autor_enviado || !titulo_enviado) {
+        return res
+            .status(400)
+            .json({ mensagem: "dsAutor e dsTitulo são obrigatórios" });
+    }
+
+    let id_novo = ultimo_id + 1;
+
+    let novo_livro = {
+        idLivro: id_novo,
+        fgDisponivel: true,
+        dsTitulo: titulo_enviado,
+        dsAutor: autor_enviado,
+    };
+
+    livros.push(novo_livro);
+    ultimo_id = id_novo;
+
+    console.log(novo_livro);
+
+    res.status(201).json(novo_livro);
+});
 
 app.listen(3000, () => {
     console.log("Servidor rodando na porta 3000");
 });
-
-// URL certa pra testar:
-// http://localhost:3000/produtos?categoria=eletronicos
